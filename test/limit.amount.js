d4326f73cc74aa319db3f343b7ac51b292455be0
lstrichashl <lstrichashl@gmail.com>
2017-03-13 19:04:18 +0200

    add tests for amount

---
+++ test/limit.amount.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var  request = require('supertest');
+var redis = require('redis');
+var OdysseusLimiter = require('../index');
+
+describe('limit', function(){
+    var store, app;
+    var options, redis_client;
+    before(function(){
+        redis_client = redis.createClient({host: 'localhost', port: 6379});
+        store = new OdysseusLimiter.RedisStore({
+            host: 'localhost',
+            port: 6379
+        });
+    });
+    beforeEach(function(){
+        app = express();
+        options = {
+            store: store,
+            key: function (req) {
+                return req.query.key;
+            },
+            amount: function(req){
+                return 0;
+            },
+            interval: function (req) {
+                return 1000000;
+            }
+        };
+        redis_client.send_command('flushall', function (err, res) {
+            if(err) throw err;
+        })
+    });
+    describe('.amount', function(){
+        describe('change requests amount', function(){
+            it('should block after 0 requests', function(done){
+                app.use(OdysseusLimiter.limit(options));
+                app.get('/', function(req, res, next){
+                    res.status(200).json({status: 'ok'});
+                });
+                request(app)
+                    .get('/?key=limit.amount.blockAfter0')
+                    .expect(429, done);
+
+            });
+            it('should block after 1 requests', function(done){
+                var calls = 0;
+                options.
+                    amount = function (req) {
+                        return 1;
+                    };
+                app.use(OdysseusLimiter.limit(options));
+                app.get('/', function(req, res, next){
+                    res.status(200).json({status: 'ok'});
+                });
+                request(app)
+                    .get('/?key=limit.amount.blockafter13')
+                    .expect(200, function () {
+                        request(app)
+                            .get('/?key=limit.amount.blockafter13')
+                            .expect(429, done);
+                    });
+            });
+        });
+    });
+});
