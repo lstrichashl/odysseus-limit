5a36ffd33e8f0275708ee7137a4c46df515ffca4
lstrichashl <lstrichashl@gmail.com>
2018-06-21 17:10:37 +0300

    make 'general' the default key

--- store/LocalStore.js
+++ store/LocalStore.js
@@ -7,10 +7,12 @@ function LocalStore(){
 util.inherits(LocalStore, require('./Store'));
 
 LocalStore.prototype.getRequestCount = function(key, onSucceed, onFailed){
+    key = key || "general";
     if(onSucceed) onSucceed(this.queues[key] ? this.queues[key].length : 0);
 }
 
 LocalStore.prototype.addRequest = function(request, onSucceed, onFailed){
+    request.key = request.key || "general";
     var thisQueue = this.queues;
     this.queues[request.key] = this.queues[request.key] || [];
     this.queues[request.key].push(request.key);
